8dd8d693b06f9e10db077b4c67a73f2b5200aaa9
Jasmine <jassycodes@gmail.com>
2019-02-19 18:16:40 -0800

    change server.py

--- server.py
+++ server.py
@@ -1,53 +1,39 @@
-#creating a new flask app called chatapp
-
 from flask import Flask, render_template, request, Response, make_response, jsonify, json
 from flask import redirect, url_for 
 import requests
 import os
 import re
 import html
-import sqlite3 #used to create the Connection object that represent the database
+import sqlite3
 import datetime
 
-#once we import Flask, we need to create an instance of the Flask class for our web app. That’s what line 3 does. __name__ is a special variable that gets as value the string "__main__" when you’re executing the script.
-app = Flask(__name__) 
+app = Flask(__name__)
 
-#To use the module, you must first create a Connection object that represents the database. Here the data will be stored in the chatapp.db file:
 connection = sqlite3.connect('data/chatapp.db')
 c = connection.cursor()
 
-#creating a table that has:id, message, date posted, sender id, receiver id and some foreign keys that relate to the User table below it 
 c.execute('''CREATE TABLE IF NOT EXISTS message 
 			 (id INTEGER PRIMARY KEY AUTOINCREMENT, message VARCHAR(300) NOT NULL, date_posted default CURRENT_DATE, sender_id INTEGER, receiver_id INTEGER, FOREIGN KEY(sender_id) REFERENCES users(id), FOREIGN KEY(receiver_id) REFERENCES users(id))''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS users
 			 (id INTEGER PRIMARY KEY AUTOINCREMENT, username text NOT NULL, password VARCHAR(12) NOT NULL, fname text, lname text, birthday date)''')
 
-#creating our own users as a test down below 
 # c.execute("INSERT INTO users(username, password, fname) VALUES(?,?,?);", ("jassycodes", "polygloter03", "Jasmine"))
 # c.execute("INSERT INTO users(username, password, fname) VALUES(?,?,?);", ("morsal11", "polygloter11", "Morsal"))
 
-# Save (commit) the changes
 connection.commit()
-
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
 connection.close()
 
-
-#this is the homepage route that will connect to an index.html page
 @app.route("/")
 def homepage():
 	return render_template('index.html')
 
+@app.route("/sample")
+def sample():
+	return render_template('samplechat.html')
 
-@app.route("/sample") #creating a route for localhost:5000/sample
-def sample(): #creating a function called sample
-	return render_template('samplechat.html') #connection to html - Flask will look for templates in the templates folder. 
-
-@app.route("/sendmessage", methods=['POST']) #The browser tells the server that it wants to POST some new information to that URL (/sendmessage)
-#and that the server must ensure the data is stored and only stored once. This is how HTML forms usually transmit data to the server.
-def send(): #creating a function called "send"
+@app.route("/sendmessage", methods=['POST'])
+def send():
 	sender = request.form.get('sender')
 	receiver = request.form.get('receiver')	
 	a_message = request.form.get('message')
@@ -61,21 +47,13 @@ def send(): #creating a function called "send"
 
 	sender_id = 0
 	receiver_id = 0
-<<<<<<< HEAD
-	senderFound = False #
-	receiverFound = False #
-=======
 	senderFound = False
 	receiverFound = False
 	sender_name = ""
 	receiver_name = ""
-<<<<<<< HEAD
->>>>>>> c35c11925263998fea09d52f659444acaf6927f0
-=======
 	error_msg = ""
->>>>>>> 799a04e0fe278c27c427f0d87ef78d5cdb4056eb
 
-	#query for checking if username SENDER exists in the database
+	#query for checking if username sender exists in the database
 	c.execute("SELECT id FROM users WHERE username='{}'".format(sender))
 
 	if c.fetchone() is not None:
@@ -90,7 +68,7 @@ def send(): #creating a function called "send"
 		print("Empty")
 		error_msg = " no username '" + sender + "' found"
 
-	#query for checking if username RECEIVER exists in the database
+	#query for checking if username receiver exists in the database
 	c.execute("SELECT id FROM users WHERE username='{}'".format(receiver))
 	# print(c.fetchall())
 	if c.fetchone() is not None:
